81c0144157f921b985d371e4ce0463b10645f1b4
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-04-05 20:39:26 +0300

    Güncelleme

--- client/src/pages/CreateBlog.js
+++ client/src/pages/CreateBlog.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { Box, Button, InputLabel, TextField, Typography } from "@mui/material";
-import toast from "react-hot-toast";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, InputLabel, TextField, Typography } from '@mui/material';
+import toast from 'react-hot-toast';
 const CreateBlog = () => {
-  const id = localStorage.getItem("userId");
+  const id = localStorage.getItem('userId');
   const navigate = useNavigate();
   const [inputs, setInputs] = useState({
-    title: "",
-    description: "",
-    image: "",
+    title: '',
+    description: '',
+    image: '',
   });
   // input change
   const handleChange = (e) => {
@@ -22,15 +22,15 @@ const CreateBlog = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("/api/v1/blog/create-blog", {
+      const { data } = await axios.post('/api/v1/blog/create-blog', {
         title: inputs.title,
         description: inputs.description,
         image: inputs.image,
         user: id,
       });
       if (data?.success) {
-        toast.success("Blog Created");
-        navigate("/my-blogs");
+        toast.success('Blog Created');
+        navigate('/my-blogs');
       }
     } catch (error) {
       console.log(error);
@@ -40,30 +40,20 @@ const CreateBlog = () => {
     <>
       <form onSubmit={handleSubmit}>
         <Box
-          width={"50%"}
+          width={'50%'}
           border={3}
           borderRadius={10}
           padding={3}
           margin="auto"
-          boxShadow={"10px 10px 20px #ccc"}
+          boxShadow={'10px 10px 20px #ccc'}
           display="flex"
-          flexDirection={"column"}
+          flexDirection={'column'}
           marginTop="30px"
         >
-          <Typography
-            variant="h2"
-            textAlign={"center"}
-            fontWeight="bold"
-            padding={3}
-            color="gray"
-          >
+          <Typography variant="h2" textAlign={'center'} fontWeight="bold" padding={3} color="gray">
             Create A Pots
           </Typography>
-          <InputLabel
-            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
-          >
-            Title
-          </InputLabel>
+          <InputLabel sx={{ mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }}>Title</InputLabel>
           <TextField
             name="title"
             value={inputs.title}
@@ -72,11 +62,7 @@ const CreateBlog = () => {
             variant="outlined"
             required
           />
-          <InputLabel
-            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
-          >
-            Description
-          </InputLabel>
+          <InputLabel sx={{ mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }}>Description</InputLabel>
           <TextField
             name="description"
             value={inputs.description}
@@ -85,11 +71,7 @@ const CreateBlog = () => {
             variant="outlined"
             required
           />
-          <InputLabel
-            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
-          >
-            Image URL
-          </InputLabel>
+          <InputLabel sx={{ mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }}>Image URL</InputLabel>
           <TextField
             name="image"
             value={inputs.image}
